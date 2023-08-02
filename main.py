fad744db8a383f6a24a81ab3fd899cbd954817b6
twelve271 <yehorhromov27@gmail.com>
2023-08-03 01:07:16 +0200

    v3.0

--- main.py
+++ main.py
@@ -1,35 +1,41 @@
 from interface import Ui_MainWindow
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QMainWindow, QWidget, QLabel, QLineEdit
+from PyQt5 import QtWidgets
+from PyQt5.QtGui import QIcon
 import sys
-from ctypes import *
-from ctypes.wintypes import *
 import time
 import pyautogui
 import keyboard
-from tkinter import *
-
-
 
 class Clicker(QtWidgets.QMainWindow):
     def __init__(self):
         super(Clicker, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        #self.ui.pushButton.clicked.connect(self.left)#
-        self.ui.radioButton.clicked.connect(self.left)
-        self.ui.radioButton_2.clicked.connect(self.right)
+        self.ui.start_button.clicked.connect(self.start)
+        self.initUI()
+        self.ui.lineEdit.setMaxLength(1)
 
-    def left_right_button(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-
-        self.radioButton.toggled.connect(self.left)
- 
-        self.radioButton_2.toggled.connect(self.right)
-     
-    def left(self, selected):
+    def initUI(self):
+        self.setWindowTitle('AutoClicker')
+        self.setWindowIcon(QIcon('iconnn.png'))
+        
+    def start(self):
         text_bind = self.ui.lineEdit.text()
+        if not text_bind:
+            self.ui.warning_key.setText("PROVIDE A KEY IN KEY FIELD!")
+            self.ui.warning_key.setStyleSheet("color: red")
+        elif not self.ui.left_click.isChecked() and not self.ui.right_click.isChecked():
+            self.ui.warning_key.setText("CHOOSE LMB OR RMB!")
+            self.ui.warning_key.setStyleSheet("color: red")
+        elif self.ui.left_click.isChecked():
+            self.ui.warning_key.clear()
+            self.left(text_bind)
+            print(3)
+        elif self.ui.right_click.isChecked():
+            self.ui.warning_key.clear()
+            self.right(text_bind)
+
+    def left(self,text_bind):
         while True:
             if not keyboard.is_pressed(text_bind):
                 pyautogui.click()
@@ -37,16 +43,14 @@ class Clicker(QtWidgets.QMainWindow):
             else:
                 break
              
-    def right(self, selected):
-        text_bind = self.ui.lineEdit.text()
+    def right(self,text_bind):
         while True:
             if not keyboard.is_pressed(text_bind):
                 pyautogui.click(button="right")
                 time.sleep(0.1)
             else:
                 break
-                   
-             
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
